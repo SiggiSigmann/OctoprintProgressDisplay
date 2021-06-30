4594bb86e353b2447b1e796a2adf294bf0c4edda
SiggiSigmann <siggisigmann@googlemail.com>
2021-06-30 21:50:49 +0200

    add stuff from old project

---
+++ src/main.cpp
@@ -0,0 +1,249 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <ArduinoJson.h>
+#include "secrets.h"
+
+//connection and tcp stuff
+ESP8266WiFiMulti WiFiMulti;
+StaticJsonDocument<1200> doc;
+
+//connection to shiftregister
+int shiftPin = 13;
+int storePin = 15;
+int dataPin = 2;
+
+//drawing to display
+int dot = B00000001;
+int e = 0x7C;
+int r = 0x18;
+int numbers[]= {B11101110,
+                B10000010,
+                B11011100,
+                B11010110,
+                B10110010,
+                B01110110,
+                B01111110,
+                B11000010,
+                B11111110,
+                B11110110};
+
+int ring[]= {B00000100,
+              B00001100,
+              B00101100,
+              B01101100,
+              B11101100,
+              B11101110};
+
+//octoprint
+const String apikey = APIKEY;
+const String host = HOST;
+
+//prototypes
+void printTo7Segment(int vlaue);
+void lowlineTo7Segment();
+void errorTo7Segment();
+int createnumber(int input);
+
+
+void setup() {
+  //pins
+  pinMode(storePin, OUTPUT);
+  pinMode(shiftPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(12, INPUT);
+  pinMode(14, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("start");
+
+  //init 7 segments
+  printTo7Segment( 0xFFFF );
+
+  //connect to wifi (dots of the displays will blink)
+  WiFi.begin(SSID, PASSWORD);
+  byte count = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    printTo7Segment(dot<<(count*8));
+    
+    count++;
+    if(count > 1){
+      count = 0;
+    }
+    delay(100);
+  }
+
+  //write 0
+  printTo7Segment(0);
+  
+  Serial.println(WiFi.localIP());
+}
+
+int heating[2] = {0,0};
+
+void loop() {
+  float bedTarget = 0;
+  float bedTemperature = 0;
+  float toolTarget = 0;
+  float toolTemperature = 0;
+  
+  //create clients
+  WiFiClient client;
+  HTTPClient http;
+  
+  //make an api call for temperature
+  if(http.begin(client, host + "/api/printer?apikey=" + apikey)){
+
+    //process answere
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+      if (httpCode == HTTP_CODE_OK){
+
+        //get payload
+        String payload = http.getString();
+        DeserializationError error = deserializeJson(doc, payload);
+        
+        //handel error and retry
+        if (error) {
+          http.end();
+          errorTo7Segment();
+          delay(100);
+          return;
+        }
+        
+        //pars json
+        JsonObject root = doc.as<JsonObject>();
+        bedTarget = root["temperature"]["bed"]["target"];
+        bedTemperature = root["temperature"]["bed"]["actual"];
+        toolTarget = root["temperature"]["tool0"]["target"];
+        toolTemperature = root["temperature"]["tool0"]["actual"];
+  
+        //get if heeting is on
+        heating[0] = bedTemperature<bedTarget;
+        heating[1] = toolTemperature<toolTarget;
+      }
+        
+    }else{
+      //print error
+      errorTo7Segment();
+    }
+    http.end();
+  }
+  
+  //get jobs
+  if(http.begin(client, host + "/api/job?apikey=" + apikey)){
+
+    //process answere
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+      if (httpCode == HTTP_CODE_OK){
+        String payload = http.getString();
+        DeserializationError error = deserializeJson(doc, payload);
+        
+        if (error) {
+          http.end();
+          errorTo7Segment();
+          delay(100);
+          return;
+        }
+        
+        JsonObject root = doc.as<JsonObject>();
+  
+        //check if printing
+        String state = root["state"];
+        if(state.equals("Printing")){
+
+          //get progress in percent
+          int percentageOfPrint = int(root["progress"]["completion"]);
+          
+          //check if heating is required
+          if((toolTarget==0) |( bedTarget==0) | ((toolTarget-toolTemperature)>2)){
+            //create ring for heating
+            int bednum = int((6/bedTarget)*bedTemperature);
+            int toolnum = int((6/toolTarget)*toolTemperature);
+            if((toolnum<1) |( toolnum > 7)){
+              toolnum=1;
+            }
+            if((bednum<1) | (bednum > 7)){
+              bednum=1;
+            }
+            
+            Serial.print(bednum);
+            Serial.println(toolnum);
+
+            //display information ( heating progress)
+            printTo7Segment(((ring[toolnum-1])<<8)+(ring[bednum-1]));
+          }else{
+            //display information ( print progress)
+            printTo7Segment(createnumber(percentageOfPrint));
+          }
+        }else if(state.equals("Cancelling") or state.equals("Operational") ){
+          //check if coolingdown
+          if(((abs(toolTarget-toolTemperature))>2)| ((abs(bedTarget-bedTemperature))>2)){
+            int bednum = int((6/60)*(bedTemperature));
+            int toolnum = int((6/215)*toolTemperature);
+            if(toolnum<1 | toolnum > 7){
+              toolnum=1;
+            }
+
+            if(bednum<1 | bednum > 7){
+              bednum=1;
+            }
+            
+            //print information
+            Serial.print(bednum);
+            Serial.println(toolnum);
+            printTo7Segment(((ring[toolnum-1])<<8)+(ring[bednum-1]));
+          }else{
+            //print idel status
+            lowlineTo7Segment();
+          }
+        }else{
+          //print error
+          errorTo7Segment();
+        }
+      }
+    }else{
+      //print error
+      errorTo7Segment();
+    }
+    http.end();
+  }
+  delay(500);
+}
+
+//print value to 7 segment display
+void printTo7Segment(int vlaue){
+  digitalWrite(storePin, LOW);
+  shiftOut(dataPin, shiftPin, MSBFIRST, (vlaue & 0xFF) | heating[0]);
+  shiftOut(dataPin, shiftPin, MSBFIRST, ((vlaue & 0xFF00)>>8)| heating[1]);
+  digitalWrite(storePin, HIGH);
+}
+
+//print "ER" to display
+void errorTo7Segment(){
+  digitalWrite(storePin, LOW);
+  shiftOut(dataPin, shiftPin, MSBFIRST, r| heating[0]);
+  shiftOut(dataPin, shiftPin, MSBFIRST, e| heating[1]);
+  digitalWrite(storePin, HIGH);
+}
+
+//print "__" to diaply
+void lowlineTo7Segment(){
+  digitalWrite(storePin, LOW);
+  shiftOut(dataPin, shiftPin, MSBFIRST, 0x4| heating[0]);
+  shiftOut(dataPin, shiftPin, MSBFIRST, 0x4| heating[1]);
+  digitalWrite(storePin, HIGH);
+}
+
+//create number to print
+int createnumber(int input){
+  int zehn = (input / 10)%100;
+  int zehnZahl = numbers[zehn]<<8;
+  
+  int einz = input % 10;
+  int einzZahl = numbers[einz];
+  
+  return zehnZahl | einzZahl;
+}
